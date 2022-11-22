7871941237c9acd9198abcec69dc09a1b83fb566
deuscx <1475453714@qq.com>
2022-11-22 21:50:17 +0800

    feat: add markdown page

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
 import './style.css'
+import 'uno.css'
+import { createRouter, createWebHistory } from 'vue-router'
+import routes from '~pages'
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+const router = createRouter({
+    routes,
+    history: createWebHistory()
+})
+createApp(App).use(router).mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+import Unocss from 'unocss/vite'
+import Pages from 'vite-plugin-pages'
+import Markdown from 'vite-plugin-vue-markdown'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  resolve: {
+    alias: [
+      { find: '~/', replacement: `${resolve(__dirname, 'src')}/` },
+    ],
+  },
+  plugins: [
+    vue({
+      // For Vue Markdown Plugin
+      include: [/\.vue$/, /\.md$/],
+    }),
+    Unocss({ /* options */ }),
+
+    Pages({
+      extensions: ['vue', 'md'],
+    }),
+    
+    Markdown()
+  ]
 })
